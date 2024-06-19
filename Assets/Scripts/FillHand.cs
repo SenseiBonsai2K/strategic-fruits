fd104d2138cc6a21270704524de09ab0b2f09e79
SenseiBonsai2K <cristian.marinozzi1@gmail.com>
2024-06-19 17:20:00 +0200

    Hand: Implemented the Hand filling of the cards

---
+++ Assets/Scripts/FillHand.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class FillHand : MonoBehaviour
+{
+    public GameObject cardPrefab; // The card prefab
+    public List<GameObject> cardsInHand; // The list of cards in hand
+    public float cardOffset; // The offset between cards
+    public float yOffset; // The offset in the y direction to prevent overlap
+
+    void Start()
+    {
+        // Fill the hand with cards
+        for (int i = 0; i < 12; i++)
+        {
+            GameObject card = Instantiate(cardPrefab, transform, false);
+            card.transform.localPosition = new Vector3(cardOffset * (i - 5.5f), i * yOffset, -(i * yOffset / 2));
+            card.transform.Rotate(Vector3.right, 40);
+            cardsInHand.Add(card);
+        }
+    }
+}
