3ed49cd9fd65a3be480da1779bc5e19dc36e3c53
macmat01 <matteo.machella01@gmail.com>
2024-06-24 14:14:03 +0200

    Simplification

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,21 +1,24 @@
-﻿using UnityEngine;
+﻿#region
 
+using UnityEngine;
+
+#endregion
 /// <summary>
 ///     Represents a playing card.
 /// </summary>
-public class Card
+public sealed class Card
 {
     /// <summary>
     ///     Gets or sets the rank of the card.
     /// </summary>
     /// <value>The rank of the card.</value>
-    public int Rank { get; set; }
+    public int Rank { get; internal set; }
 
     /// <summary>
     ///     Gets or sets the suit of the card.
     /// </summary>
     /// <value>The suit of the card.</value>
-    public string Suit { get; set; }
+    public string Suit { get; internal set; }
 
     /// <summary>
     ///     Gets or sets the content of the card.
