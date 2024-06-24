f63873fb787bff23cd9822334427eae8e1d5a530
macmat01 <matteo.machella01@gmail.com>
2024-06-24 12:19:45 +0200

    Added Unit Tests for FillHand class

--- Assets/Scripts/FillHand.cs
+++ Assets/Scripts/FillHand.cs
@@ -1,78 +1,129 @@
-﻿using UnityEngine;
+﻿#region
 
-public class FillHand : MonoBehaviour
+using JetBrains.Annotations;
+using UnityEngine;
+
+#endregion
+public sealed class FillHand : MonoBehaviour
 {
-    public GameObject cardPrefab;
-    public float cardOffset, yOffset, zOffset;
-    public Card[] Cards { get; private set; }
+    private const float XOffset = 0.15f;
+    private const float YOffset = 0.008f;
+    private const float ZOffset = 0.002f;
 
-    private void Start()
+    public GameObject CardPrefab;
+    public Card[] Cards
+    {
+        get;
+        private set;
+    }
+
+    public void Start()
     {
         Cards = GenerateCard();
 
-        for (var i = 0; i < 12; i++)
+        for (int i = 0; i < 12; i++)
         {
-            var card = Instantiate(cardPrefab, transform, false);
-            card.name = Cards[i].Rank + " of " + Cards[i].Suit; // Use Rank as name for example
+            GameObject card = Instantiate(CardPrefab, transform, false);
+            card.name = Cards[i].Rank + " of " + Cards[i].Suit; // Use Rank as name, for example
             card.tag = gameObject.tag;
             card.transform.localPosition =
-                new Vector3(cardOffset * (i - 5.5f), yOffset - i * yOffset, zOffset + i * zOffset);
+                new Vector3((XOffset * (i - 5.5f)), (YOffset - i * YOffset), (ZOffset + i * ZOffset));
             card.transform.Rotate(Vector3.right, 35);
         }
     }
 
+    [NotNull]
     private Card[] GenerateCard()
     {
         return new[]
         {
             new Card
             {
-                Rank = 1, Suit = gameObject.tag, Content = Resources.Load<Texture>("Textures/1 " + gameObject.tag)
+                Rank = 1,
+                Suit = gameObject.tag,
+                Content = Resources.Load<Texture>("Textures/1 " + gameObject.tag)
             },
             new Card
             {
-                Rank = 1, Suit = gameObject.tag, Content = Resources.Load<Texture>("Textures/1 " + gameObject.tag)
+                Rank = 1,
+                Suit = gameObject.tag,
+                Content = Resources.Load<Texture>("Textures/1 " + gameObject.tag)
             },
             new Card
             {
-                Rank = 1, Suit = gameObject.tag, Content = Resources.Load<Texture>("Textures/1 " + gameObject.tag)
+                Rank = 1,
+                Suit = gameObject.tag,
+                Content = Resources.Load<Texture>("Textures/1 " + gameObject.tag)
             },
             new Card
             {
-                Rank = 1, Suit = gameObject.tag, Content = Resources.Load<Texture>("Textures/1 " + gameObject.tag)
+                Rank = 1,
+                Suit = gameObject.tag,
+                Content = Resources.Load<Texture>("Textures/1 " + gameObject.tag)
             },
             new Card
             {
-                Rank = 2, Suit = gameObject.tag, Content = Resources.Load<Texture>("Textures/2 " + gameObject.tag)
+                Rank = 2,
+                Suit = gameObject.tag,
+                Content = Resources.Load<Texture>("Textures/2 " + gameObject.tag)
             },
             new Card
             {
-                Rank = 2, Suit = gameObject.tag, Content = Resources.Load<Texture>("Textures/2 " + gameObject.tag)
+                Rank = 2,
+                Suit = gameObject.tag,
+                Content = Resources.Load<Texture>("Textures/2 " + gameObject.tag)
             },
             new Card
             {
-                Rank = 2, Suit = gameObject.tag, Content = Resources.Load<Texture>("Textures/2 " + gameObject.tag)
+                Rank = 2,
+                Suit = gameObject.tag,
+                Content = Resources.Load<Texture>("Textures/2 " + gameObject.tag)
             },
             new Card
             {
-                Rank = 3, Suit = gameObject.tag, Content = Resources.Load<Texture>("Textures/3 " + gameObject.tag)
+                Rank = 3,
+                Suit = gameObject.tag,
+                Content = Resources.Load<Texture>("Textures/3 " + gameObject.tag)
             },
             new Card
             {
-                Rank = 3, Suit = gameObject.tag, Content = Resources.Load<Texture>("Textures/3 " + gameObject.tag)
+                Rank = 3,
+                Suit = gameObject.tag,
+                Content = Resources.Load<Texture>("Textures/3 " + gameObject.tag)
             },
             new Card
             {
-                Rank = 4, Suit = gameObject.tag, Content = Resources.Load<Texture>("Textures/4 " + gameObject.tag)
+                Rank = 4,
+                Suit = gameObject.tag,
+                Content = Resources.Load<Texture>("Textures/4 " + gameObject.tag)
             },
             new Card
             {
-                Rank = 4, Suit = gameObject.tag, Content = Resources.Load<Texture>("Textures/4 " + gameObject.tag)
+                Rank = 4,
+                Suit = gameObject.tag,
+                Content = Resources.Load<Texture>("Textures/4 " + gameObject.tag)
             },
             new Card
             {
-                Rank = 5, Suit = gameObject.tag, Content = Resources.Load<Texture>("Textures/5 " + gameObject.tag)
+                Rank = 5,
+                Suit = gameObject.tag,
+                Content = Resources.Load<Texture>("Textures/5 " + gameObject.tag)
             }
         };
+    }
+
+    public static float GetXOffset()
+    {
+        return XOffset;
+    }
+
+    public static float GetYOffset()
+    {
+        return YOffset;
+    }
+
+    public static float GetZOffset()
+    {
+        return ZOffset;
     }
 }
