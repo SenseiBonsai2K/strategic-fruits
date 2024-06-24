c9378e2a5d84462ed3f3a3ab5f116467b75908ba
SenseiBonsai2K <cristian.marinozzi1@gmail.com>
2024-06-24 13:01:12 +0200

    Documented and Refactored

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,8 +1,25 @@
 ﻿using UnityEngine;
 
+/// <summary>
+///     Represents a playing card.
+/// </summary>
 public class Card
 {
+    /// <summary>
+    ///     Gets or sets the rank of the card.
+    /// </summary>
+    /// <value>The rank of the card.</value>
     public int Rank { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the suit of the card.
+    /// </summary>
+    /// <value>The suit of the card.</value>
     public string Suit { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the content of the card.
+    /// </summary>
+    /// <value>The content of the card.</value>
     public Texture Content { get; set; }
 }
