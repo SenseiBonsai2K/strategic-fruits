f97daef6e865351e6502c26faec842f5138f35d8
SenseiBonsai2K <cristian.marinozzi1@gmail.com>
2024-06-25 14:38:04 +0200

    Documented and refactored

--- Assets/Scripts/CardTracker.cs
+++ Assets/Scripts/CardTracker.cs
@@ -1,20 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+///     The CardTracker class is responsible for tracking the cards played in a round and the time elapsed since the start
+///     of the round.
+/// </summary>
 public class CardTracker : MonoBehaviour
 {
+    /// <summary>
+    ///     The time at which the current round started.
+    /// </summary>
     private static float _roundStartTime;
-    private static List<(Card, float)> PlayedCards = new List<(Card, float)>();
 
+    /// <summary>
+    ///     A list of tuples, each containing a Card that was played and the time elapsed since the start of the round when the
+    ///     card was played.
+    /// </summary>
+    private static readonly List<(Card, float)> PlayedCards = new();
+
+    /// <summary>
+    ///     Saves a card and the time elapsed since the start of the round to the PlayedCards list.
+    /// </summary>
+    /// <param name="card">The card to be saved.</param>
     public static void SaveCardAndTime(Card card)
     {
-        float timeElapsed = Time.time - _roundStartTime;
+        var timeElapsed = Time.time - _roundStartTime;
         PlayedCards.Add((card, timeElapsed));
 
         Debug.Log($"Card saved: {card.Rank} of {card.Suit}, Time elapsed: {timeElapsed}");
     }
-    
+
+    /// <summary>
+    ///     Clears the PlayedCards list and resets the round start time to the current time.
+    /// </summary>
     public static void ClearRoundData()
     {
         PlayedCards.Clear();
